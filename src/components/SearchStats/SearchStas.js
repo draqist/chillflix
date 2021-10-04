0f1d122a9a8554a2942cd4c951825e363c69484b
Draqode <Boomjay008@gmail.com>
2021-10-04 09:12:57 +0100

    new changes added to the stat-results div

--- src/components/SearchStats/SearchStas.js
+++ src/components/SearchStats/SearchStas.js
@@ -7,35 +7,49 @@ const StasContainer = () => {
             <div className='statscontainer'>
                 <h3> Search Results </h3>
                 <ul className = 'stats-ul'>
-                    <li>
-                        <a href='/'> Movies</a>
-                        <span>  a </span>
-                    </li>
-                    <li>
-                        <a href='/'>TV Shows</a>
-                        <span> a </span>
-                    </li>
-                    <li>
-                        <a href='/'>People</a>
-                        <span> a </span>
-                        
-                    </li>
-                    <li>
-                        <a href='/'>Companies</a>
-                        <span> a </span>
-                    </li>
-                    <li>
-                        <a href='/'>Keywords</a>
-                        <span> a </span>
-                    </li>
-                    <li>
-                        <a href='/'>Collections</a>
-                        <span> a </span>
-                    </li>
-                    <li>
-                        <a href='/'>Networks</a>
-                        <span> a </span>
-                    </li>
+                    <div className = 'res_cat'>
+                        <li>
+                            <a href='/'> Movies</a>
+                            <span>  a </span>
+                        </li>
+                    </div>
+                    <div>
+                        <li>
+                            <a href='/'>TV Shows</a>
+                            <span> a </span>
+                        </li>
+                    </div>
+                    <div>
+                        <li>
+                            <a href='/'>People</a>
+                            <span> a </span>
+                            
+                        </li>
+                    </div>
+                    <div>
+                        <li>
+                            <a href='/'>Companies</a>
+                            <span> a </span>
+                        </li>
+                    </div>
+                    <div>
+                        <li>
+                            <a href='/'>Keywords</a>
+                            <span> a </span>
+                        </li>
+                    </div>
+                    <div>
+                        <li>
+                            <a href='/'>Collections</a>
+                            <span> a </span>
+                        </li>
+                    </div>
+                    <div>
+                        <li>
+                            <a href='/'>Networks</a>
+                            <span> a </span>
+                        </li>
+                    </div>
                 </ul>
             </div> 
         </>
