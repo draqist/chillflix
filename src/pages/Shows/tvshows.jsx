37308278d8a25d1d1b43c92fcf02ee906f6ca1c0
Draqode <Boomjay008@gmail.com>
2021-10-07 22:55:10 +0100

    tests101

--- src/pages/Shows/tvshows.jsx
+++ src/pages/Shows/tvshows.jsx
@@ -2,36 +2,39 @@ import React from 'react';
 import ImageMain from '../../components/ImageMain/imagemain'
 import './tvshows.scss'
 import {  } from '../../constants/constants'
-import MovieRec1 from '../../components/MovieRec1/movierec1';
+import SeriesDetails from '../../components/MovieRec1/movierec1';
 
 function TvShows () {
     return (
-        <div className= 'main__container'>
+        <div className= 'mainz__container'>
             <div className='container-2'>
                 <ImageMain />
             </div>
-
             <div className='categories'>
                 <div className="series__no">
                     <h2> No of Seasons </h2>
+                    <ul>
+                        <li>cd</li>
+                        <li>ab</li>
+                    </ul>
                 </div>
                 <div className="series__info">
                     <h2> Useful  Information</h2>
                     <h5> Useful  Information is an information determined to be useful by the user or the manufacturer. They are the guidlines containing the do and dont's constituting an establishment.
                     </h5>
                     <div className='card-container'>
-                        <MovieRec1/>
-                        <MovieRec1/>
-                        <MovieRec1/>
-                        <MovieRec1/>
-                        <MovieRec1/>
-                        <MovieRec1/>
-                        <MovieRec1/>
-                        <MovieRec1/>
-                        <MovieRec1/>
-                        <MovieRec1/>
-                        <MovieRec1/>
-                        <MovieRec1/>
+                        <SeriesDetails/>
+                        <SeriesDetails/>
+                        <SeriesDetails/>
+                        <SeriesDetails/>
+                        <SeriesDetails/>
+                        <SeriesDetails/>
+                        <SeriesDetails/>
+                        <SeriesDetails/>
+                        <SeriesDetails/>
+                        <SeriesDetails/>
+                        <SeriesDetails/>
+                        <SeriesDetails/>
                     </div>
                 </div>
             </div>
