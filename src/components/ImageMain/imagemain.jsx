a44b693537638f04e3b5bb5fde3d2eec39341c74
Draqode <Boomjay008@gmail.com>
2021-10-20 03:27:28 +0100

    made some sleek changes and also trying to format the air date

--- src/components/ImageMain/imagemain.jsx
+++ src/components/ImageMain/imagemain.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './imagemain.scss'
+import moment from 'moment'
 
-const ImageMain = ({img})=> {
-
+const ImageMain = ({ img, name, date }) => {
+    // let air_date = moment({ date }).format('MMM DD, YYYY')
+    let mate = moment({date}, ["YYYY-MM-DD", moment.ISO_8601]).format('MMM DD, YYYY')
+    console.log(mate)
     return (
-        <img src = {"https://image.tmdb.org/t/p/w500/" + img } className='img_container-1' alt='/' />
+        <div className = 'img-shows-desk'>
+            <img src={"https://image.tmdb.org/t/p/w500/" + img} className='img_container-1' alt='/' />
+            <h5> {name}</h5>
+            {/* <h6> {air_date}</h6> */}
+            
+        </div>
         );
     }
         
--- src/components/Ser-Results/SerResults.js
+++ src/components/Ser-Results/SerResults.js
@@ -2,6 +2,15 @@ import React, {useState, useEffect} from 'react'
 import '../../pages/Shows/tvshows.scss'
 import './SerResults.scss'
 import ImageMain from '../ImageMain/imagemain'
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import { ThemeProvider } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
+import moment from 'moment'
 
 const SerResults = () => {
     const [series, setSeries] = useState([])
@@ -11,12 +20,37 @@ const SerResults = () => {
             .then(newresponse => setSeries(newresponse.results))
     }
     useEffect(() => SeriesDetFetcher(), [])
+    const theme = createTheme({
+        typography: {
+            fontFamily: 'Montserrat'
+        },
+});
     return (
         <div className='ser__results'>
-            {
-                series.map(ser => <ImageMain key={ser.id.toString()} img={ser.poster_path} details={ser.overview} name={ser.name} date={ser.first_air_date} />)
-            }
-            <div className='container-2'>
+            <GlobalStyles styles={{ fontFamily: 'Montserrat' }} />
+            <div className='show-sorters'>
+                <ThemeProvider theme = {theme}>
+                    <Accordion>
+                        <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                        >
+                        <Typography>Sort Results By</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                        <Typography>
+                            <li>Google </li>
+                        </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                </ThemeProvider>
+            </div>
+            <div className = 'sorted-shows'>
+                    {
+                        series.map(ser => <ImageMain key={ser.id.toString()} img={ser.poster_path} details={ser.overview} name={ser.name} date={ser.first_air_date.toString()} />)
+                }
+                <span> Lucifer </span>
             </div>
         </div>
     )
