c1b78493d75f85d6444a8fb36ecff2801211d220
Draqode <Boomjay008@gmail.com>
2021-11-21 10:28:56 +0100

    hopeful

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,26 +1,26 @@
-import React, {useState, useEffect, useCallback} from 'react'
+import React, {useState} from 'react'
 import './SearchBox.scss'
 
 const SearchBox = () => {
-    const [value, setValue] = useState('a')
+    const [value, setValue] = useState('')
   // const [searchresults, setSearchResults] = useState([])
   // console.log(searchresults)
     const SearchChangeHandler = (e) => {
         setValue(e.target.value)
     }
-    const SearchFetch = useCallback(
-        () => {
-            fetch(`https://api.themoviedb.org/3/search/multi?api_key=98750334fac1aaa94aca2b7a98d59728&language=en-US&query=${value}&page=1&include_adult=false`)
-            .then(res => res.json())
-            .then(searchres => {
-                if (searchres){
-                    // setSearchResults(searchres.results)
-                } 
-        })
-        },
-        [value],
-    )
-    useEffect(() => SearchFetch(), [SearchFetch])
+    // const SearchFetch = useCallback(
+    //     () => {
+    //         fetch(`https://api.themoviedb.org/3/search/multi?api_key=98750334fac1aaa94aca2b7a98d59728&language=en-US&query=${value}&page=1&include_adult=false`)
+    //         .then(res => res.json())
+    //         .then(searchres => {
+    //             if (searchres){
+    //                 // setSearchResults(searchres.results)
+    //             } 
+    //     })
+    //     },
+    //     [value],
+    // )
+    // useEffect(() => SearchFetch(), [SearchFetch])
     return (
         <div className = 'search-field'>
             <form>
